8674b78a3ce54c0d109d68f62fd4058268c4a9b1
Tyler Mitchell <tyler.mitchell.97@outlook.com>
2025-01-13 03:32:45 -0500

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   title: "Lotse",
   description: "Minimalistic dark mode blogging template",
-  baseURL: "https://lotse.vercel.app",
+  baseURL: "https://gfxmitchell.github.io",
   darkBackground: "#1a1a1a",
   resume: "/resume.pdf", // upload your resume in the /public folder
   showResume: true,
